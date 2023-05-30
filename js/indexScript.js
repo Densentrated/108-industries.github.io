ac409300883f032bd2632c16015be01bdbe9e0c6
Densentrated <72484574+Densentrated@users.noreply.github.com>
2023-05-29 21:51:48 -0700

    got some animations done

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,5 +1,21 @@
+// header and subheadr movement
 anime({
     targets: ".header",
-    translateX: 120,
-    durration: 1600,
+    translateX: ["-40%", "4%"],
+    durration: 20000,
+    easeing: "easeInOutSine",
+    delay: anime.stagger(140)
 })
+
+anime({
+    targets: "#subHead1",
+    translateX:"14%",
+    duration: 1000
+})
+
+anime({
+    targets: "#subHead2",
+    translateX:"24%",
+    durration: 1000
+})
+
